a04035192c44f0ba60855bc19d9f0bf35bd80151
vaqueas <vaqueas@gmail.com>
2015-07-05 11:52:28 -0400

    MSDA'15 Home Work

---
+++ HW1.sql
@@ -0,0 +1,13 @@
+﻿--Answer 1:
+select * from flights;
+--Answer 2:
+select carrier, count(*) TotalFlights from flights group by carrier;
+--Answer 3:
+select carrier, count(*) TotalFlights from flights group by carrier order by TotalFlights;
+--Answer 4:
+select carrier, count(*) TotalFlights from flights group by carrier order by TotalFlights limit 5;
+--Answer 5:
+select carrier, count(*) TotalFlights from flights where distance>=1000 group by carrier order by TotalFlights limit 5;
+--Answer 6:
+select origin, max(dep_delay) max_dep_delayed_airport from flights group by origin order by max(dep_delay);
+
